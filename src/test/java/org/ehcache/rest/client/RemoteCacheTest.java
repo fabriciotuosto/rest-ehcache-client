879453aaf31b9ea1aa68356efeacbd7945c852c1
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2010-01-17 22:24:58 -0300

    fixed RemoteCacheTest

--- src/test/java/org/ehcache/rest/client/RemoteCacheTest.java
+++ src/test/java/org/ehcache/rest/client/RemoteCacheTest.java
@@ -1,14 +1,14 @@
 package org.ehcache.rest.client;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.ehcache.rest.client.Cache;
-import org.ehcache.rest.client.CacheLocation;
-import org.ehcache.rest.client.Md5HashGenerator;
-import org.ehcache.rest.client.ByteArrays;
-import org.ehcache.rest.client.RemoteCache;
-import org.ehcache.rest.client.RestClient;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,7 +27,7 @@ public class RemoteCacheTest {
     private Cache cache;
     private static CacheLocation location;
     private Md5HashGenerator hasher = new Md5HashGenerator(new ByteArrays());
-	private RestClient restTemplate;
+	private RestClient restClient;
 
     @BeforeClass
     public static void createLocation(){
@@ -36,28 +36,22 @@ public class RemoteCacheTest {
 	
 	@Before
     public void setup() {
-        restTemplate = mock(RestClient.class);    
-        cache = new RemoteCache(location,restTemplate,hasher);
+        restClient = mock(RestClient.class);    
+        cache = new RemoteCache(location,restClient,hasher);
     }  
 
     @Test
     public void testGetOnMissingValue() {
-//    	when(response.isSuccessful()).thenReturn(false);
-//    	when(response.readObject()).thenReturn(null);
-//        assertNull("Empty cache contains a value", cache.get("not-there"));
+    	when(restClient.get(any(String.class))).thenReturn(null);
+        assertNull("Empty cache contains a value", cache.get("not-there"));
     }
 
     @Test
     public void testPutStringIsRetrievable() {
-//    	when(executor.execute(any(PutMethod.class))).thenReturn(response);
-//    	when(response.isSuccessful()).thenReturn(false);
-//    	
-//    	HttpResponseInterpreter response2 = mock(HttpResponseInterpreter.class);
-//    	when(executor.execute(any(GetMethod.class))).thenReturn(response2);
-//    	when(response2.readObject()).thenReturn("test-value");
-//    	
-//    	cache.put("test-key1", "test-value");
-//        assertEquals("Value not retrieved from cache", "test-value", cache.get("test-key1"));
+    	when(restClient.put(any(String.class),eq("test-value"))).thenReturn(true);
+    	when(restClient.get(any(String.class))).thenReturn("test-value");
+    	cache.put("test-key1", "test-value");
+        assertEquals("Value not retrieved from cache", "test-value", cache.get("test-key1"));
     }
 
     @Test(expected=IllegalArgumentException.class)
@@ -86,10 +80,8 @@ public class RemoteCacheTest {
 
     @Test
     public void testDeletionOfNonExistentItem() {
-//    	when(executor.execute(any(DeleteMethod.class))).thenReturn(response);
-//    	when(response.isSuccessful()).thenReturn(false);
-//    	
-//        assertFalse("Deletion of missing item should be false", cache.delete("missing-key"));
+    	when(restClient.delete(any(String.class))).thenReturn(false);
+        assertFalse("Deletion of missing item should be false", cache.delete("missing-key"));
     }
 }
 
