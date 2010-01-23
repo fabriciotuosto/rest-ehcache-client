21b99b251ea07600b0f3d0ed581009362ff300c9
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2010-01-23 19:49:48 -0300

    Added creation method to provide cache creation on a different url than the preconfigured in spring context

--- src/main/java/org/ehcache/rest/client/CacheFactory.java
+++ src/main/java/org/ehcache/rest/client/CacheFactory.java
@@ -6,39 +6,47 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * 
  * @author Fabricio
- *
+ * 
  */
 public class CacheFactory {
-	
+
 	/**
 	 * 
 	 */
-    private static final String CACHE_BEAN = "cache";
+	private static final String CACHE_BEAN = "cache";
+	private static final String REST_CLIENT_BEAN = "restClient";
+	private static final String HASH_GENERATOR = "md5hashGenerator";
 
-    /**
-     * 
-     * @return
-     */
-    public Cache getCache(){
-		return (Cache) getContext().getBean(CACHE_BEAN);
-	}
-	
 	/**
-	 * 	
+	 * 
 	 * @return
 	 */
-	private ApplicationContext getContext(){
+	public Cache getCache() {
+		return (Cache) getContext().getBean(CACHE_BEAN);
+	}
+
+	public Cache getCache(CacheURI location) {
+		return new RemoteCache(location, getContext().getBean(REST_CLIENT_BEAN,
+				RestClient.class), getContext().getBean(HASH_GENERATOR,
+				HashGenerator.class));
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private ApplicationContext getContext() {
 		return ContextHolder.CONTEXT;
 	}
-	
+
 	/**
 	 * 
 	 *
 	 */
-	private static class ContextHolder{
-        private static final String SPRING_CONTEXT_CONFIGURATION = "cache-service.xml";
-        private static final ApplicationContext CONTEXT = new ClassPathXmlApplicationContext(
-				new String[] {SPRING_CONTEXT_CONFIGURATION});
-    }
-	
+	private static class ContextHolder {
+		private static final String SPRING_CONTEXT_CONFIGURATION = "cache-service.xml";
+		private static final ApplicationContext CONTEXT = new ClassPathXmlApplicationContext(
+				new String[] { SPRING_CONTEXT_CONFIGURATION });
+	}
+
 }
